c688b9048cff82d58287ce76d43303ec80cc79ea
MuhammadYassir <62455195+MuhammadYassir@users.noreply.github.com>
2020-04-13 09:31:08 +0800

    Operator Switch disasah ayam

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,24 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int Jalan2=2;
+        switch(Jalan2){
+            case 1:
+                System.out.println("Galuh Banjar");
+                break;
+            case 2:
+                System.out.println("disasah ayam");
+                break;
+            case 3:
+                System.out.println("Kamar hibak nyamuk");
+                break;
+            case 4:
+                System.out.println("kucing kelaparan");
+                break;
+            case 5:
+                System.out.println("Hedeh");
+                break;
+        }
+    }
+}
