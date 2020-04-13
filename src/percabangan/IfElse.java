17b6c3d98e90966a56c83a1076bf74ee84265a72
MuhammadYassir <62455195+MuhammadYassir@users.noreply.github.com>
2020-04-13 09:12:52 +0800

    Operator IFELSE TRUE

---
+++ src/percabangan/IfElse.java
@@ -0,0 +1,14 @@
+package percabangan;
+
+public class IfElse {
+    public static void main(String[] args) {
+        double totalbelanja = 70000;
+        double uangDompet = 35000;
+
+        if(uangDompet<totalbelanja){
+            System.out.println("Uang Kurang");
+        }else{
+            System.out.println("Uang Cukup");
+        }
+    }
+}
