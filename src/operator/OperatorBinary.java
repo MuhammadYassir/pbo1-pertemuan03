55b9efce0e0715ef6e270562d1037651950e28b7
MuhammadYassir <62455195+MuhammadYassir@users.noreply.github.com>
2020-04-13 08:43:55 +0800

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -3,4 +3,12 @@ package operator;
 public class OperatorBinary {
     String nama = "Muhammad Zida Yassir";
     int jumlahSks = 24;
+
+    public static void main(String[] args) {
+        System.out.println(10+10);
+        System.out.println(10-7);
+        System.out.println(10*6);
+        System.out.println(10/8);
+        System.out.println(10%5);
+    }
 }
