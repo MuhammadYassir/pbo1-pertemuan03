446e78ea22c3b4c065f0037d33414b8d4c95d785
MuhammadYassir <62455195+MuhammadYassir@users.noreply.github.com>
2020-04-13 08:54:11 +0800

    Operator Unary

---
+++ src/operator/OperatorUnary.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class OperatorUnary {
+    public static void main(String[] args) {
+        int i = 2;
+        i++;
+        System.out.println(i);
+        i++;
+        System.out.println(i);
+        i=i+1;
+        System.out.println(i);
+        i*=5;
+        System.out.println(i);
+    }
+}
