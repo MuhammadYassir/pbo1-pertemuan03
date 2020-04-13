86755ed4a965d6ade20d125697bb64106f5f850a
MuhammadYassir <62455195+MuhammadYassir@users.noreply.github.com>
2020-04-13 10:19:07 +0800

    Operator Switch percakapan

--- src/percabangan/Switch.java
+++ src/percabangan/Switch.java
@@ -2,23 +2,26 @@ package percabangan;
 
 public class Switch {
     public static void main(String[] args) {
-        int Jalan2=2;
-        switch(Jalan2){
-            case 1:
-                System.out.println("Galuh Banjar");
+        char nilai='D';
+        switch(nilai){
+            case 'A':
+                System.out.println("Mhs     :\"Terima kasih banyak pak\"");
+                System.out.println("Dosen   :\"Iya Sama-sama !\"");
                 break;
-            case 2:
-                System.out.println("disasah ayam");
+            case 'B':
+                System.out.println("Mhs     :\"Kenapa ulun kada dapat A pak\"");
+                System.out.println("Dosen   :\"Kenapa yooo??\"");
                 break;
-            case 3:
-                System.out.println("Kamar hibak nyamuk");
+            case 'C':
+                System.out.println("Mhs     :\"Ulun ngmpul tarus tugas pa\"");
+                System.out.println("Dosen   :\"iyalah\"");
                 break;
-            case 4:
-                System.out.println("kucing kelaparan");
-                break;
-            case 5:
-                System.out.println("Hedeh");
-                break;
+            default:
+                System.out.println("Mhs     :\"Bapa!\"");
+                System.out.println("Dosen   :\"Kenapa??\"");
+                System.out.println("Mhs     :\"Kpp,kd jadi wkwkwk\"");
+                System.out.println("Dosen   :\"Nginih handak cari masalah kah!?\"");
+                System.out.println("Mhs     :\"Hehehe... :'( \"");
         }
     }
 }
